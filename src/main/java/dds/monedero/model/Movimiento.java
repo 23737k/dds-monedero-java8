0807b6bf964bcb57cd8eaee84e06fdf9b4455243
kenti <mopup2017@gmail.com>
2021-05-14 16:09:06 -0300

    refactor type test

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -2,17 +2,15 @@ package dds.monedero.model;
 
 import java.time.LocalDate;
 
-public class Movimiento {
+public abstract class Movimiento {
   private LocalDate fecha;
   //En ningún lenguaje de programación usen jamás doubles para modelar dinero en el mundo real
   //siempre usen numeros de precision arbitraria, como BigDecimal en Java y similares
   private double monto;
-  private boolean esDeposito;
-
-  public Movimiento(LocalDate fecha, double monto, boolean esDeposito) {
+ 
+  public Movimiento(LocalDate fecha, double monto) {
     this.fecha = fecha;
     this.monto = monto;
-    this.esDeposito = esDeposito;
   }
 
   public double getMonto() {
@@ -22,37 +20,50 @@ public class Movimiento {
   public LocalDate getFecha() {
     return fecha;
   }
-
-  public boolean fueDepositado(LocalDate fecha) {
-    return isDeposito() && esDeLaFecha(fecha);
-  }
-
-  public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
-  }
-
+  
   public boolean esDeLaFecha(LocalDate fecha) {
-    return this.fecha.equals(fecha);
-  }
-
-  public boolean isDeposito() {
-    return esDeposito;
-  }
-
-  public boolean isExtraccion() {
-    return !esDeposito;
-  }
-
+	    return this.fecha.equals(fecha);
+	  }
+  
   public void agregateA(Cuenta cuenta) {
-    cuenta.setSaldo(calcularValor(cuenta));
-    cuenta.agregarMovimiento(fecha, monto, esDeposito);
-  }
-
-  public double calcularValor(Cuenta cuenta) {
-    if (esDeposito) {
-      return cuenta.getSaldo() + getMonto();
-    } else {
-      return cuenta.getSaldo() - getMonto();
-    }
-  }
+	    cuenta.setSaldo(calcularValor(cuenta));
+	    cuenta.agregarMovimiento(fecha, monto);
+	  }
+  
+  public abstract double calcularValor(Cuenta cuenta);
+  
 }
+
+class Extraccion extends Movimiento{
+	
+	public Extraccion(LocalDate fecha, double monto) {
+		super(fecha, monto);
+	}
+
+	public double calcularValor(Cuenta cuenta) {
+		 return cuenta.getSaldo() - getMonto();
+	}
+	
+	public boolean fueExtraido(LocalDate fecha) {
+	    return esDeLaFecha(fecha);
+	  }
+}
+
+
+
+class Deposito extends Movimiento{
+
+	public Deposito(LocalDate fecha, double monto) {
+		super(fecha, monto);
+	}
+
+	public double calcularValor(Cuenta cuenta) {
+		return cuenta.getSaldo() + getMonto();
+	}
+	
+	public boolean fueDepositado(LocalDate fecha) {
+	    return esDeLaFecha(fecha);
+	  }
+	
+}
+
